c23c1f5bd5228b4038270d7079ea3ba0885cba32
brunolemos06 <bruno.c.lemos@sapo.pt>
2023-04-05 01:21:18 +0100

    ridemate

--- ridemate/lib/pages/login_page.dart
+++ ridemate/lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'CreateAccountPage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,47 +8,87 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final _url = 'http://10.0.2.2:3000/'; // replace with your URL
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        children: [
+          SizedBox(height: 75), // empty space at the top
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Email and Password sign-in
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    ElevatedButton(
-                      onPressed: () async {
-                        // Perform sign-in logic
-                      },
-                      child: Text('Sign in'),
-                    ),
-                  ],
+              SizedBox(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Login'),
                 ),
               ),
-            ]
-
+              SizedBox(width: 20),
+              SizedBox(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CreateAccountPage(),
+                      ),
+                    );
+                  },
+                  child: Text('Create Account'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 400,
+            child: WebView(
+              initialUrl: _url,
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
       ),
     );
