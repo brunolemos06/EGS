865a8e029461b9a1a8b09ea3d34cfb17c276117f
Tiago <tamarques@ua.pt>
2023-05-24 11:24:46 +0100

    fix

--- create_images.sh
+++ create_images.sh
@@ -1,27 +1,29 @@
+reg_port=5432
+
 #auth_backend
 cd auth/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/auth_backend:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/auth_backend:v1 .
 
 #auth_frontend
 cd frontend/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/auth_frontend:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/auth_frontend:v1 .
 
 #chat_api
 cd ../../chat_api/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/chat_api:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/chat_api:v1 .
 
 #composer
 cd ../composer/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/composer:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/composer:v1 .
 
 #payments
 cd ../payments/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/payments:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/payments:v1 .
 
 #reviews
-cd ../reviews/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/reviews:v1 .
+cd ../review/
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/reviews:v1 .
 
 #trip
 cd ../trip/
-docker buildx build --platform linux/amd64 --network=host -t registry.deti:5000/egs-ridemate/trip:v1 .
+docker buildx build --platform linux/amd64 --network=host -t registry.deti:$reg_port/egs-ridemate/trip:v1 .
--- push_images.sh
+++ push_images.sh
@@ -1,7 +1,8 @@
-docker push registry.deti:5000/egs-ridemate/auth_backend:v1
-docker push registry.deti:5000/egs-ridemate/auth_frontend:v1
-docker push registry.deti:5000/egs-ridemate/chat_api:v1
-docker push registry.deti:5000/egs-ridemate/composer:v1
-docker push registry.deti:5000/egs-ridemate/payments:v1
-docker push registry.deti:5000/egs-ridemate/reviews:v1
-docker push registry.deti:5000/egs-ridemate/trip:v1
+reg_port=5432
+docker push registry.deti:$reg_port/egs-ridemate/auth_backend:v1
+docker push registry.deti:$reg_port/egs-ridemate/auth_frontend:v1
+docker push registry.deti:$reg_port/egs-ridemate/chat_api:v1
+docker push registry.deti:$reg_port/egs-ridemate/composer:v1
+docker push registry.deti:$reg_port/egs-ridemate/payments:v1
+docker push registry.deti:$reg_port/egs-ridemate/reviews:v1
+docker push registry.deti:$reg_port/egs-ridemate/trip:v1
