af914e1dd9e23a4370dbc9bae83950550572ce91
Claudio <claudioasensio@ua.pt>
2023-06-06 15:30:14 +0100

    chat fix

--- chat_api/chat.py
+++ chat_api/chat.py
@@ -10,13 +10,13 @@ from flask import jsonify
 from flask_swagger import swagger
 
 
-account_sid="ACf8b3ce0eb04f21fe7ab30a6e0c58c7d9"
-auth_token= "4cb96b0112a701609a3b621bc403af76"
-service_sid="ISec5512aaa2db4d6abaf1bd31a60fa474"
+# account_sid="ACf8b3ce0eb04f21fe7ab30a6e0c58c7d9"
+# auth_token= "4cb96b0112a701609a3b621bc403af76"
+# service_sid="ISec5512aaa2db4d6abaf1bd31a60fa474"
 
-# account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-# auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-# service_sid = os.getenv("TWILIO_SERVICE_SID")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+service_sid = os.getenv("TWILIO_SERVICE_SID")
 client = Client(account_sid, auth_token)
 
 configuration = client.conversations \
